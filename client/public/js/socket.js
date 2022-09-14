fd31f6029b6af33556fd672382641d19557b0c0c
jahona <jahona5240@gmail.com>
2022-09-14 17:58:26 +0900

    웹소켓 통신 스크립트 추가

--- client/public/js/socket.js
+++ client/public/js/socket.js
@@ -1,9 +1,24 @@
 const socket = new WebSocket('ws://localhost:8080');
 
 socket.addEventListener('open', function (event) {
-    socket.send('Hello Server!');
+    socket.send('Ready For Chatting');
 });
 
 socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
+    try {
+        let payload = JSON.parse(event.data);
+        console.log(payload);
+
+        if (payload.type == 'received_otherUser_message') {
+            window.dispatchEvent(new CustomEvent('receiveOtherUserMessage', {
+                "detail": payload.content
+            }));
+        }
+    } catch (err) {
+
+    }
 });
+
+function ws_sendMessage(message) {
+    socket.send(message);
+}
